d982148527957a119ad5f139ef332cc76b82bc7f
AK-Malik <akmalik.it@gmail.com>
2024-12-24 14:52:33 -0500

    Local message

--- src/main/java/com/example/Barclays/apply/CoreApply/CoreApplyApplication.java
+++ src/main/java/com/example/Barclays/apply/CoreApply/CoreApplyApplication.java
@@ -8,7 +8,7 @@ public class CoreApplyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreApplyApplication.class, args);
-		System.out.println ("Hello Anil");
+		System.out.println ("Hello Mr. Anil");
 	}
 
 }
