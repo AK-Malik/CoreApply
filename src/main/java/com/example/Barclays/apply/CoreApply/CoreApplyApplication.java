6deea374416691735bcb2d12b0367f771e715fa9
AK-Malik <akmalik.it@gmail.com>
2024-12-24 13:06:12 -0500

    Local message

---
+++ src/main/java/com/example/Barclays/apply/CoreApply/CoreApplyApplication.java
@@ -0,0 +1,14 @@
+package com.example.Barclays.apply.CoreApply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreApplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplyApplication.class, args);
+		System.out.println ("Hello Anil");
+	}
+
+}
---
+++ src/test/java/com/example/Barclays/apply/CoreApply/CoreApplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Barclays.apply.CoreApply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
